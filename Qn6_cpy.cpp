1f1cfbc3eb97c15e1bb07e83849bf443f2c4c9ec
Ronalds3 <147844460+Ronalds3@users.noreply.github.com>
2025-04-03 14:11:34 +0300

    Add files via upload

---
+++ Qn6_cpy.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <iomanip>
+
+int main() {
+  double num1, num2, result;
+  char operation;
+  char continueCalc;
+
+  do {
+    std::cout << "Enter first number, operator, second number: ";
+    std::cin >> num1 >> operation >> num2;
+
+    std::cout << std::fixed << std::setprecision(6);
+
+    switch (operation) {
+      case '+':
+        result = num1 + num2;
+        break;
+      case '-':
+        result = num1 - num2;
+        break;
+      case '*':
+        result = num1 * num2;
+        break;
+      case '/':
+        if (num2 != 0) {
+          result = num1 / num2;
+        } else {
+          std::cout << "Error: Division by zero!" << std::endl;
+          continue; // Skip to the next iteration
+        }
+        break;
+      default:
+        std::cout << "Error: Invalid operator!" << std::endl;
+        continue; // Skip to the next iteration
+    }
+
+    std::cout << "Answer = " << result << std::endl;
+
+    std::cout << "Do another (y/n)? ";
+    std::cin >> continueCalc;
+
+  } while (continueCalc == 'y');
+
+  return 0;
+}
